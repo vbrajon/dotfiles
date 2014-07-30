144802b949c835b560ce0885162e8d0acddffb14
Valentin Brajon <vbrajon@gmail.com>
2014-07-30 19:33:11 +0200

    Update README

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 - zsh
 
 ## Steps
-- Install fasd / git / tmux / vim / zsh
+- Install git / tmux / vim / zsh
 - Clone the project : `git clone https://github.com/vbrajon/dotfiles .dotfiles`
-- Make symbolic links : `cd .dotfiles && ./symlinks.sh`
+- Backup old config
+- Make symbolic links : `cd .dotfiles && ./init.sh`
--- init.sh
+++ init.sh
@@ -1,11 +1,32 @@
 #/bin/bash
 
 # Z
-mkdir -p ~/.z
-curl https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.z.sh
+ls ~/.z.sh || (curl https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.z.sh)
 
 # Vim
 mkdir -p ~/.vim/tmp
 mkdir -p ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ls ~/.vim/bundle/Vundle.vim || git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Symlinks
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+rm ~/.atom/config.cson
+rm ~/.atom/keymap.cson
+rm ~/.gitconfig
+rm ~/.gitexcludes
+rm ~/.tmux.conf
+rm ~/.vimrc
+rm ~/.zsh
+rm ~/.zshrc
+
+ln -s $SCRIPTPATH/atom/config.cson ~/.atom/config.cson
+ln -s $SCRIPTPATH/atom/keymap.cson ~/.atom/keymap.cson
+ln -s $SCRIPTPATH/gitconfig ~/.gitconfig
+ln -s $SCRIPTPATH/gitexcludes ~/.gitexcludes
+ln -s $SCRIPTPATH/tmux.conf ~/.tmux.conf
+ln -s $SCRIPTPATH/vimrc ~/.vimrc
+ln -s $SCRIPTPATH/zsh ~/.zsh
+ln -s $SCRIPTPATH/zshrc ~/.zshrc
 
