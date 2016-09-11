aab32455f62061ce92495e05680493b69308e0b5
Valentin Brajon <vbrajon@gmail.com>
2016-09-11 13:30:54 +0200

    Add s

--- zsh/vbrajon-shortcuts.zsh
+++ zsh/vbrajon-shortcuts.zsh
@@ -16,24 +16,21 @@ then
   alias git=hub
 fi
 
-# zz cmd [arguments]
-# If the first argument is an existing file
-# Then execute "cmd arguments"
-# Else move "z arguments" and execute "cmd ."
+# zz [cmd] [path] [args]
+# If path is not an existing file then execute "z path"
+# Then execute "cmd args"
 zz() {
   cmd=$1
   shift
-  if [ -e "$1" ]
+  if ! [ -e "$1" ] && ! [[ $# -eq 0 ]]
   then
-    # echo "no z, exec $cmd $@"
-    eval "$cmd $@"
-  else
-    # echo "z, exec z $@ then $cmd"
-    silent z $@
-    eval "$cmd ."
+    zpath=$1
+    shift
   fi
+  # echo "z $zpath && $cmd ${@:-.}"
+  silent z $zpath
+  eval "$cmd ${@:-.}"
 }
-
 
 # t
 # If already in a tmux session, do nothing
@@ -46,15 +43,40 @@ t() {
     tmux select-window -t 1
     tmux split-window -h
     tmux split-window -v
-    tmux select-pane -t 1.2
-    tmux select-pane -t 1.1
+    tmux split-window -v
+    tmux split-window -v
     tmux send-keys -t 1.2 'tmux clock-mode' Enter
     tmux send-keys -t 1.3 'htop' Enter
+    tmux resize-pane -t 1.2 -U 1000
+    tmux resize-pane -t 1.2 -R 1000
+    tmux resize-pane -t 1.2 -L 50
+    tmux resize-pane -t 1.2 -D 8
+    tmux select-pane -t 1.1
   fi
   tmux attach-session -t yeah
 }
 # Run tmux
 t
+
+h() {
+  if [[ $# -eq 0 ]]
+  then
+    history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n10
+  else
+    history | grep $@
+  fi
+}
+
+server() {
+  re='^[0-9]+$'
+  [[ $1 =~ $re ]] && port=$1 || port=8000
+  if [ "$port" -gt 1024 ]
+  then
+    python -m SimpleHTTPServer $port
+  else
+    sudo python -m SimpleHTTPServer $port
+  fi
+}
 
 alias g=git
 alias v=vim
@@ -62,4 +84,5 @@ alias vi=vim
 alias a='zz atom'
 alias o='zz open'
 alias m='zz meteor'
+alias s='zz server'
 alias dotfiles='curl -L https://raw.githubusercontent.com/vbrajon/dotfiles/master/install.sh | bash'
