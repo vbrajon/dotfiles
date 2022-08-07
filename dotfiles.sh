60ef49cdd57ec672bf8a02b149f369470094c811
Valentin Brajon <valentin.brajon@neoxam.com>
2022-08-07 17:06:24 +0200

    feat: clean + add rust alternative commands

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,4 +1,4 @@
-log() { echo "\033[32m$@\033[0m"; }
+log() { echo -e $yellow$@$reset; }
 
 which brew &>/dev/null || {
   log "> Install Package Manager"
@@ -14,15 +14,23 @@ ls ~/.dotfiles &>/dev/null || {
 
 [[ -f ~/.extra ]] || {
   log "> Configure Dotfiles"
+  PACKAGES="
+brew install node docker brave-browser visual-studio-code microsoft-office
+npm install -g http-server
+"
   read -p "Name: ($NAME) " NAME
   read -p "Email: ($EMAIL) " EMAIL
   read -p "Hostname: ($HOSTNAME) " HOSTNAME
-  read -p "Override Mac Preferences (Y/n)" MAC
+  read -p "Install Additional Packages:$PACKAGES(Y/n) " PKG
+  read -p "Install Mac Preferences (Y/n) " MAC
+  [[ $PKG ]] || PKG=Y
+  [[ $MAC ]] || MAC=Y
   [[ $HOSTNAME ]] || HOSTNAME=$(hostname)
   cat > ~/.extra <<EOL
 NAME="$NAME"
 EMAIL="$EMAIL"
 HOSTNAME="$HOSTNAME"
+PACKAGES="$PACKAGES"
 EOL
   cat > ~/.gitextra <<EOL
 [user]
@@ -37,11 +45,16 @@ EOL
 }
 
 [[ $(date -r ~/.extra "+%H%M") == "0000" ]] || {
-  log "> Install Packages"
-  touch -t $(date +%Y%m%d0000) ~/.dotfiles/.extra
-  source ~/.dotfiles/.extra
+  log "> Install Required Packages"
+  touch -t $(date +%Y%m%d0000) ~/.extra
+  source ~/.extra
+
+  brew leaves | xargs [[ " ${array[*]} " =~ " ${value} " ]] && echo "true" || echo "false"
+
   brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
-  brew install bash bash-completion@2 git tmux htop fzf fd ripgrep diff-so-fancy
+  brew install bash bash-completion@2 git git-delta tmux
+  brew install bat btop duf dust exa fd fzf ripgrep
+  brew install httpie sampler
   brew tap homebrew/cask-fonts
   brew install font-hack
 
@@ -51,57 +64,63 @@ EOL
   defaults write com.apple.terminal "Default Window Settings" "Raw"
   defaults write com.apple.terminal "Startup Window Settings" "Raw"
 
-  [[ "$MAC" != [Yy]* ]] && break
-  log "> Update Mac Preferences"
-  osascript -e 'tell application "System Preferences" to quit'
-  sudo -v;while true;do sudo -n true;sleep 60;kill -0 "$$" || exit;done 2>/dev/null &
-  scutil --set HostName "$HOSTNAME"
-  scutil --set LocalHostName "$HOSTNAME"
-  scutil --set ComputerName "$HOSTNAME"
-  dscacheutil -flushcache
-  /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
-  chflags nohidden /Volumes
-  chflags nohidden ~/Library
-  hash tmutil &> /dev/null && tmutil disable
-  nvram SystemAudioVolume=" "
-  touch ~/.bash_sessions_disable
-  defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
-  defaults write com.apple.dashboard mcx-disabled -bool true
-  defaults write com.apple.desktopservices DSDontWriteNetworkStores true
-  defaults write com.apple.dock autohide -bool true
-  defaults write com.apple.dock autohide-delay -float 0
-  defaults write com.apple.dock autohide-time-modifier -float 0
-  defaults write com.apple.dock expose-animation-duration -float 0.1
-  defaults write com.apple.dock expose-group-by-app -bool false
-  defaults write com.apple.dock launchanim -bool false
-  defaults write com.apple.dock mru-spaces -bool false
-  defaults write com.apple.dock showhidden -bool true
-  defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-  defaults write com.apple.finder _FXSortFoldersFirst -bool true
-  defaults write com.apple.finder DisableAllAnimations -bool true
-  defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-  defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-  defaults write com.apple.finder FXInfoPanesExpanded -dict General -bool true OpenWith -bool true Privileges -bool true
-  defaults write com.apple.finder QuitMenuItem -bool true
-  defaults write com.apple.finder ShowPathbar -bool true
-  defaults write com.apple.finder ShowStatusBar -bool true
-  defaults write com.apple.finder WarnOnEmptyTrash -bool false
-  defaults write com.apple.ImageCapture disableHotPlug -bool true
-  defaults write com.apple.LaunchServices LSQuarantine -bool false
-  defaults write com.apple.screencapture disable-shadow -bool true
-  defaults write com.apple.screencapture location -string "$HOME/Desktop"
-  defaults write com.apple.screencapture type -string "png"
-  defaults write com.apple.terminal StringEncodings -array 4
-  defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-  defaults write NSGlobalDomain com.apple.springing.delay -float 0
-  defaults write NSGlobalDomain com.apple.springing.enabled -bool true
-  defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-  defaults write NSGlobalDomain InitialKeyRepeat -int 10
-  defaults write NSGlobalDomain KeyRepeat -int 5
-  defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
-  defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-  defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
-  defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-  defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+  [[ "$PKG" == [Yy]* ]] && {
+    log "> Install Additional Packages"
+    echo "$PACKAGES" | bash
+  }
+
+  [[ "$MAC" == [Yy]* ]] && {
+    log "> Install Mac Preferences"
+    osascript -e 'tell application "System Preferences" to quit'
+    sudo -v;while true;do sudo -n true;sleep 60;kill -0 "$$" || exit;done 2>/dev/null &
+    scutil --set HostName "$HOSTNAME"
+    scutil --set LocalHostName "$HOSTNAME"
+    scutil --set ComputerName "$HOSTNAME"
+    dscacheutil -flushcache
+    /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+    chflags nohidden /Volumes
+    chflags nohidden ~/Library
+    hash tmutil &> /dev/null && tmutil disable
+    nvram SystemAudioVolume=" "
+    touch ~/.bash_sessions_disable
+    defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+    defaults write com.apple.dashboard mcx-disabled -bool true
+    defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+    defaults write com.apple.dock autohide -bool true
+    defaults write com.apple.dock autohide-delay -float 0
+    defaults write com.apple.dock autohide-time-modifier -float 0
+    defaults write com.apple.dock expose-animation-duration -float 0.1
+    defaults write com.apple.dock expose-group-by-app -bool false
+    defaults write com.apple.dock launchanim -bool false
+    defaults write com.apple.dock mru-spaces -bool false
+    defaults write com.apple.dock showhidden -bool true
+    defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+    defaults write com.apple.finder _FXSortFoldersFirst -bool true
+    defaults write com.apple.finder DisableAllAnimations -bool true
+    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+    defaults write com.apple.finder FXInfoPanesExpanded -dict General -bool true OpenWith -bool true Privileges -bool true
+    defaults write com.apple.finder QuitMenuItem -bool true
+    defaults write com.apple.finder ShowPathbar -bool true
+    defaults write com.apple.finder ShowStatusBar -bool true
+    defaults write com.apple.finder WarnOnEmptyTrash -bool false
+    defaults write com.apple.ImageCapture disableHotPlug -bool true
+    defaults write com.apple.LaunchServices LSQuarantine -bool false
+    defaults write com.apple.screencapture disable-shadow -bool true
+    defaults write com.apple.screencapture location -string "$HOME/Desktop"
+    defaults write com.apple.screencapture type -string "png"
+    defaults write com.apple.terminal StringEncodings -array 4
+    defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+    defaults write NSGlobalDomain com.apple.springing.delay -float 0
+    defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+    defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+    defaults write NSGlobalDomain InitialKeyRepeat -int 10
+    defaults write NSGlobalDomain KeyRepeat -int 5
+    defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+    defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+    defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+    defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+    defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+  }
 }
